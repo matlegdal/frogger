94ff587078d0ed7437ccfd6ac0eab24b12daa7cc
matlegdal <matlegdal@hotmail.com>
2018-01-18 08:38:03 -0500

    replace hard coded numbers by constants

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+const WIDTH = 5;
+const HEIGHT = 6;
+const STEP_X = 101;
+const STEP_Y = 83;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,26 +11,22 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = -101;
-    this.y = (Math.floor(Math.random()*3)) * 83 + 41;
-    this.speed = Math.floor(Math.random() * 100) + 100;
+    this.x = -STEP_X;
+    this.y = (Math.floor(Math.random()*3)) * STEP_Y + STEP_Y/2;
+    this.speed = Math.floor(Math.random() * 200) + 100;
+    // console.log(this.x, this.y);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += this.speed*dt;
 
-
-    if (this.x > 505) {
+    if (this.x > STEP_X*WIDTH) {
         var i = allEnemies.indexOf(this);
         allEnemies.splice(i, 1);
         allEnemies.push(new Enemy());
     }
-    // console.log("enemy at ("+this.x+ " " +this.y+")")
 };
 
 // Draw the enemy on the screen, required method for game
@@ -39,10 +40,8 @@ Enemy.prototype.render = function() {
 
 var Player = function () {
     this.avatar = 'images/char-boy.png';
-    this.x = 202;
-    this.y = 332+41;
-    this.speedx = 101;
-    this.speedy = 83;
+    this.x = STEP_X*2;
+    this.y = STEP_Y*4 + STEP_Y/2;
 };
 
 Player.prototype.update = function() {
@@ -56,20 +55,19 @@ Player.prototype.render = function () {
 Player.prototype.handleInput = function (dir) {
     switch (dir) {
         case 'left':
-            this.x -= this.speedx;
+            this.x -= STEP_X;
             break;
         case 'right':
-            this.x += this.speedx;
+            this.x += STEP_X;
             break;
         case 'up':
-            this.y -= this.speedy;
+            this.y -= STEP_Y;
             break;
         case 'down':
-            this.y += this.speedy;
+            this.y += STEP_Y;
     }
-    console.log("x: "+this.x);
-    console.log("y: "+this.y);
-
+    console.log(this.x, this.y);
+    // console.log("y: "+this.y);
 };
 
 // Now instantiate your objects.
@@ -78,7 +76,7 @@ Player.prototype.handleInput = function (dir) {
 var allEnemies = [];
 for (var i=0; i<3; i++) {
     allEnemies.push(new Enemy());
-    console.log(allEnemies[i].speed)
+    // console.log(allEnemies[i].speed)
 }
 var player = new Player();
 
